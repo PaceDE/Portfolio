dbe0e43eb77869f33d17576b8885a2d402ca9f04
Dipesh Shrestha <14depace@gmail.com>
2026-03-25 11:54:27 +0545

    revalidate 60 isntad of no-cache to prevent build error during static site genration

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,6 +1,6 @@
 export async function fetchData<T>(url: string): Promise<T | null> {
-    try {
-    const res = await fetch(url, { cache: "no-cache" })
+  try {
+    const res = await fetch(url, { next: { revalidate: 60 } })
     return res.json()
   } catch (err) {
     console.error('Fetch error:', err)
