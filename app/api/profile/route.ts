c8f6554336ddf216d16789c465af91f102ebca22
Dipesh Shrestha <14depace@gmail.com>
2026-03-25 12:17:13 +0545

    test-delpoyment-edge-case

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -3,7 +3,17 @@ import { getProfile } from '@/lib/db'
 
 export const GET = async (req: NextRequest) => {
   try {
-    const result = await getProfile();
+    const user = {
+      id:1,
+      name:"dsfs",
+      email:"sdfsd",
+      github:"sdfs",
+      linked_in:"dsfds",
+      tagline:["dfsdf"],
+      bio:"ddsf",
+      working:true
+    }
+    const result = {...user};
     return NextResponse.json(result, { status: 200 })
   } catch (err: any) {
     const message = err.message ?? "Failed to fetch data"
